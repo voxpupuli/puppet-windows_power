9b0b2904df3888a2ad115080cc1cc2d479bc771d
Dennis Hoppe <github@debian-solutions.de>
2016-10-21 16:57:18 +0200

    Fix several rubocop issues

--- spec/defines/global/battery_spec.rb
+++ spec/defines/global/battery_spec.rb
@@ -2,67 +2,69 @@ require 'spec_helper'
 
 describe 'windows_power::global::battery', type: :define do
   describe 'installing with invalid setting' do
-    let :title do 'sound off' end
-    let :params do
+    let(:title) { 'sound off' }
+    let(:params) do
       { setting: 'xxx', status: 'on' }
     end
 
     it do
-      expect {
+      expect do
         should contain_exec('set batteryalarm xxx')
-      }.to raise_error(Puppet::Error) { |e| expect(e.to_s).to match 'The setting argument does not match a valid batteryalarm setting' }
+      end.to raise_error(Puppet::Error, %r{The setting argument does not match a valid batteryalarm setting})
     end
   end
 
   describe 'installing with invalid status' do
-    let :title do 'sound off' end
-    let :params do
+    let(:title) { 'sound off' }
+    let(:params) do
       { setting: 'sound', status: 'xxx' }
     end
 
     it do
-      expect {
+      expect do
         should contain_exec('set batteryalarm sound')
-      }.to raise_error(Puppet::Error) { |e| expect(e.to_s).to match 'The status argument is not valid for sound' }
+      end.to raise_error(Puppet::Error, %r{The status argument is not valid for sound})
     end
   end
 
   describe 'installing with invalid criticality' do
-    let :title do 'sound off' end
-    let :params do
+    let(:title) { 'sound off' }
+    let(:params) do
       { setting: 'sound', status: 'on', criticality: 'xxx' }
     end
 
     it do
-      expect {
+      expect do
         should contain_exec('set batteryalarm sound')
-      }.to raise_error(Puppet::Error) { |e| expect(e.to_s).to match 'The status argument does not match: LOW or HIGH' }
+      end.to raise_error(Puppet::Error, %r{The status argument does not match: LOW or HIGH})
     end
   end
 
   ['Windows XP', 'Windows Server 2003', 'Windows Server 2003 R2'].each do |os|
     describe 'installing setting sound' do
-      let :title do 'sound' end
-      let :facts do
+      let(:title) { 'sound' }
+      let(:facts) do
         { operatingsystemversion: os }
       end
-      let :params do
+      let(:params) do
         { setting: 'sound', status: 'on' }
       end
 
-      it { should contain_exec('set batteryalarm sound').with(
-        'command' => 'C:\Windows\System32\powercfg.exe /batteryalarm LOW /sound on')
-      }
+      it do
+        should contain_exec('set batteryalarm sound').with(
+          'command' => 'C:\Windows\System32\powercfg.exe /batteryalarm LOW /sound on'
+        )
+      end
     end
   end
 
   ['Windows Vista', 'Windows 7', 'Windows 8', 'Windows Server 2008', 'Windows Server 2008 R2', 'Windows Server 2012'].each do |os|
     describe 'installing setting sound' do
-      let :title do 'sound' end
-      let :facts do
+      let(:title) { 'sound' }
+      let(:facts) do
         { operatingsystemversion: os }
       end
-      let :params do
+      let(:params) do
         { setting: 'sound', status: 'on' }
       end
 
