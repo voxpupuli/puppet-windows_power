127b0c8cd15df79108053f8b0f9c1b80e998c4ca
WeiterWeiterFertigstellen <143704302+WeiterWeiterFertigstellen@users.noreply.github.com>
2026-07-06 10:57:53 +0200

    hibernation_enabled.rb: remove redundant block

--- lib/facter/hibernation_enabled.rb
+++ lib/facter/hibernation_enabled.rb
@@ -8,15 +8,13 @@ Facter.add(:hibernation_enabled) do
   # to check if hibernation is active (1 = hibernation on, everything else = hibernation off)
 
   setcode do
-    begin
-      key = 'SYSTEM\\CurrentControlSet\\Control\\Power'
-      value = nil
-      Win32::Registry::HKEY_LOCAL_MACHINE.open(key, Win32::Registry::KEY_READ) do |reg|
-        value = reg['HibernateEnabled']
-      end
-      value == 1
-    rescue Win32::Registry::Error
-      false
+    key = 'SYSTEM\\CurrentControlSet\\Control\\Power'
+    value = nil
+    Win32::Registry::HKEY_LOCAL_MACHINE.open(key, Win32::Registry::KEY_READ) do |reg|
+      value = reg['HibernateEnabled']
     end
+    value == 1
+  rescue Win32::Registry::Error
+    false
   end
 end
