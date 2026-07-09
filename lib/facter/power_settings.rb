b708c5c1bc3919b54bc39efb9788aa512ee0c51a
Lightning <lightning@lightning-online.de>
2026-07-09 16:38:32 +0200

    improve fact power_settings
    - use symbols as hash keys
    - use ruby's capability of splitting paragraphs
      this is more sophisticated as it handles multiple empty lines and/or
      empty trailing lines better than manually splitting strings
    - catch potential problems in paragraph/line parsing

--- lib/facter/power_settings.rb
+++ lib/facter/power_settings.rb
@@ -8,24 +8,23 @@ Facter.add(:power_settings) do
 
   setcode do
     output = Facter::Core::Execution.execute('powercfg /query')
-    data_blocks = output.split(%r{\n\n})
 
     result = {
-      'monitor-timeout-ac' => 0,
-      'monitor-timeout-dc' => 0,
-      'disk-timeout-ac' => 0,
-      'disk-timeout-dc' => 0,
-      'standby-timeout-ac' => 0,
-      'standby-timeout-dc' => 0,
-      'hibernate-timeout-ac' => 0,
-      'hibernate-timeout-dc' => 0,
+      'monitor-timeout-ac': 0,
+      'monitor-timeout-dc': 0,
+      'disk-timeout-ac': 0,
+      'disk-timeout-dc': 0,
+      'standby-timeout-ac': 0,
+      'standby-timeout-dc': 0,
+      'hibernate-timeout-ac': 0,
+      'hibernate-timeout-dc': 0,
     }
 
     hex = '[a-f0-9]'
     guid_re = "#{hex}{8}-#{hex}{4}-#{hex}{4}-#{hex}{4}-#{hex}{12}"
 
     # Split the output of "powercfg /query" into blocks (empty line = new block begins):
-    data_blocks.each do |block|
+    output.each_line('', chomp: true) do |block|
       guid = ''
       lines = block.lines(chomp: true)
       lines.each do |line|
@@ -38,8 +37,8 @@ Facter.add(:power_settings) do
       # Example output:
       # Index der aktuellen Wechselstromeinstellung: 0x00000002
       # Index der aktuellen Gleichstromeinstellung: 0x00000002
-      ac = lines[-2].split(': ')[1].to_i(0) # AC in seconds
-      dc = lines[-1].split(': ')[1].to_i(0) # DC in seconds
+      ac = lines[-2].split(': ')[1].nil? ? 0 : lines[-2].split(': ')[1].to_i(0) # AC in seconds
+      dc = lines[-1].split(': ')[1].nil? ? 0 : lines[-1].split(': ')[1].to_i(0) # DC in seconds
 
       # "powercfg /aliases" displays a list of aliases and their corresponding GUIDs:
       # 0012ee47-9041-4b5d-9b77-535fba8b1442  SUB_DISK
@@ -51,17 +50,17 @@ Facter.add(:power_settings) do
       # 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e    VIDEOIDLE     -> monitor-timeout-ac/-dc
       case guid
       when '3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e' # VIDEOIDLE
-        result['monitor-timeout-ac'] = ac
-        result['monitor-timeout-dc'] = dc
+        result[:'monitor-timeout-ac'] = ac
+        result[:'monitor-timeout-dc'] = dc
       when '6738e2c4-e8a5-4a42-b16a-e040e769756e' # DISKIDLE
-        result['disk-timeout-ac'] = ac
-        result['disk-timeout-dc'] = dc
+        result[:'disk-timeout-ac'] = ac
+        result[:'disk-timeout-dc'] = dc
       when '29f6c1db-86da-48c5-9fdb-f2b67b1f44da' # STANDBYIDLE
-        result['standby-timeout-ac'] = ac
-        result['standby-timeout-dc'] = dc
+        result[:'standby-timeout-ac'] = ac
+        result[:'standby-timeout-dc'] = dc
       when '9d7815a6-7ee4-497e-8888-515a05f02364' # HIBERNATEIDLE
-        result['hibernate-timeout-ac'] = ac
-        result['hibernate-timeout-dc'] = dc
+        result[:'hibernate-timeout-ac'] = ac
+        result[:'hibernate-timeout-dc'] = dc
       end
     end
     result
